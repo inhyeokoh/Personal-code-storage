ea546ec68e2a7de7bb435eb1c3752fabb664fe0c
인혁 <111099840+inhyeokoh@users.noreply.github.com>
2024-05-27 11:39:12 +0900

    Update UI_ItemSlot.cs

--- UI_ItemSlot.cs
+++ UI_ItemSlot.cs
@@ -92,7 +92,7 @@ public class UI_ItemSlot : UI_Entity
                     {
                         // 버릴 아이템 이름 + 수량 적는 팝업
                         _inven.dropCountConfirmPanel.SetActive(true);
-                        _inven.dropCountConfirmPanel.transform.GetChild(0).GetComponent<UI_DropCountConfirm>().ChangeText(index);
+                        _inven.dropCountConfirmPanel.transform.GetChild(0).GetComponent<UI_DropCountConfirm>().ChangeText(UI_DropCountConfirm.Enum_DropUIParent.Inven, index);
                     }
                 }
             }
@@ -120,19 +120,27 @@ public class UI_ItemSlot : UI_Entity
             _inven.StopRestrictItemDescrPos(data);
         };
 
-        // 우클릭으로 아이템 장착
+        // 아이템 우클릭
         _entities[(int)Enum_UI_ItemSlot.IconImg].ClickAction = (PointerEventData data) =>
         {
-            if (CheckItemNull())
-            {
-                return;
-            }
+            if (CheckItemNull()) return;
 
-            if (data.button == PointerEventData.InputButton.Right && _invenItems[index].itemType == Enum_ItemType.Equipment) // 장비에 우클릭 한 경우
+            if (data.button == PointerEventData.InputButton.Right)
             {
-                // TODO 장착 불가 경우
-
-                GameManager.Inven.EquipItem(index);
+                // 상점 열려 있는 상태면 상점 판매탭 물품으로 이동
+                if (GameManager.UI.Shop.activeSelf)
+                {
+                    GameManager.UI.Shop.GetComponent<UI_Shop>().panel_U_Buttons[1].isOn = true; // 판매탭 활성화
+                    GameManager.Inven.InvenToShop(index);
+                }
+                else
+                {
+                    if (_invenItems[index].itemType == Enum_ItemType.Equipment) // 장비에 우클릭 한 경우
+                    {
+                        // TODO 장착 불가 경우
+                        GameManager.Inven.EquipItem(index);
+                    }
+                }
             }
         };
     }
@@ -199,21 +207,24 @@ public class UI_ItemSlot : UI_Entity
         if (GameManager.Inven.items[index].itemType == Enum_ItemType.Equipment) // 장비아이템 설명
         {
             StateItemData itemData = ItemParsing.itemDatas[GameManager.Inven.items[index].id] as StateItemData;
-            int[] stats = {itemData.level, itemData.attack, itemData.defense, itemData.speed, itemData.attackSpeed, itemData.maxHp, itemData.maxMp};
-            string descLines = string.Format(GameManager.Inven.items[index].desc, $"{itemData.level}\n", $"{itemData.attack}\n", $"{itemData.defense}\n", $"{itemData.speed}\n", $"{itemData.attackSpeed}\n", $"{itemData.maxHp}\n", $"{itemData.maxMp}\n");
-            string[] lines = descLines.Split("\n");
-
-            string desc = $"{lines[0]} \n";
-            for (int i = 1; i < lines.Length - 1; i++)
+            if (itemData != null)
             {
-                if (stats[i] == 0)
-                {
-                    continue;
-                }
-                desc += $"{lines[i]} \n";
-            }
+                int[] stats = {itemData.level, itemData.attack, itemData.defense, itemData.speed, itemData.attackSpeed, itemData.maxHp, itemData.maxMp};
+                string descLines = string.Format(GameManager.Inven.items[index].desc, $"{itemData.level}\n", $"{itemData.attack}\n", $"{itemData.defense}\n", $"{itemData.speed}\n", $"{itemData.attackSpeed}\n", $"{itemData.maxHp}\n", $"{itemData.maxMp}\n");
+                string[] lines = descLines.Split("\n");
 
-            _inven.descrPanel.transform.GetChild(2).GetComponentInChildren<TMP_Text>().text = desc;
+                string desc = $"{lines[0]} \n";
+                for (int i = 1; i < lines.Length - 1; i++)
+                {
+                    if (stats[i] == 0)
+                    {
+                        continue;
+                    }
+                    desc += $"{lines[i]} \n";
+                }
+
+                _inven.descrPanel.transform.GetChild(2).GetComponentInChildren<TMP_Text>().text = desc;
+            }
         }
         else
         {
