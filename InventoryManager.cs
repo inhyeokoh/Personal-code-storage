34484f94312f928ea463df2498339787a0694f43
인혁 <111099840+inhyeokoh@users.noreply.github.com>
2024-06-04 16:02:34 +0900

    Update InventoryManager.cs
    인벤 가득참 알림 팝업 추가
    아이템 장착 해제시 스탯 미적용 추가

--- InventoryManager.cs
+++ InventoryManager.cs
@@ -171,7 +171,9 @@ public class InventoryManager : SubClass<GameManager>
             if (items[i] == null) return i;
         }
 
-        // 비어있는 슬롯 없음
+        // 인벤토리가 가득 찬 경우 처리 (공간 부족 알림 팝업)
+        GameManager.UI.OpenPopup(GameManager.UI.InGameConfirmY);
+        GameManager.UI.InGameConfirmY.ChangeText(UI_InGameConfirmY.Enum_ConfirmTypes.InvenFull);
         return -1;
     }
 
@@ -184,15 +186,13 @@ public class InventoryManager : SubClass<GameManager>
         if (acquired.itemType == Enum_ItemType.Equipment)
         {
             int emptySlotIndex = EmptySlot;
-            if (emptySlotIndex != -1)
+            if (emptySlotIndex == -1)
             {
-                items[emptySlotIndex] = new ItemData(acquired, acquired.count);
+                return; // 인벤토리가 가득 찬 경우
             }
             else
             {
-                // 인벤토리가 가득 찬 경우 처리 (공간 부족 알림 팝업)
-                GameManager.UI.OpenPopup(GameManager.UI.InGameConfirmY);
-                GameManager.UI.InGameConfirmY.ChangeText(UI_InGameConfirmY.Enum_ConfirmTypes.InvenFull);
+                items[emptySlotIndex] = new ItemData(acquired, acquired.count);
             }
         }
         else
@@ -234,7 +234,8 @@ public class InventoryManager : SubClass<GameManager>
                     if (i == items.Count - 1)
                     {
                         int emptySlotIndex = EmptySlot;
-                        if (emptySlotIndex != -1)
+                        if (emptySlotIndex == -1) return;
+                        else
                         {
                             // 획득 수량이 최대 수량 이하인 경우
                             if (acquired.count <= acquired.maxCount)
@@ -507,7 +508,6 @@ public class InventoryManager : SubClass<GameManager>
         if (_playerInfo.gameObject.activeSelf)
         {
             _playerInfo.UpdateEquipUI(equipType);
-            _playerInfo.UpdateStatus();
         }
 
         // 인벤토리 UI 갱신
@@ -529,12 +529,6 @@ public class InventoryManager : SubClass<GameManager>
             items[index] = null;
         }
 
-        // 플레이어정보창 UI 갱신 -> TODO 이벤트 받는 형식으로 수정
-        if (_playerInfo.gameObject.activeSelf)
-        {
-            _playerInfo.UpdateStatus();
-        }
-
         inven.UpdateInvenSlot(index);
     }
 
@@ -542,6 +536,8 @@ public class InventoryManager : SubClass<GameManager>
     {
         int invenEmptySlot = EmptySlot;
         if (invenEmptySlot == -1) return; // 인벤토리 꽉 찬 경우 해제 불가
+
+        PlayerController.instance._playerEquipment.TakeOff(index);
 
         // 인벤토리 비어 있는 칸 찾아서 넣기
         items[invenEmptySlot] = equips[index];
